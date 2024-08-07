77e99e56a3ca1bc18273c4a34376be9447af381b
Ahmedd26 <ahmedabdellatife1@gmail.com>
2024-08-07 18:32:22 +0300

    Fixed some styling bugs

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,9 +2,7 @@
 module.exports = {
   content: ["./src/**/*.{html,ts}", "./node_modules/flowbite/**/*.js"],
 
-  theme: {
-    extend: {},
-  },
+  theme: {},
   plugins: [
     require("flowbite/plugin"), // add this line
   ],
